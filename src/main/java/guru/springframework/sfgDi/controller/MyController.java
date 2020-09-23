8bcfa8c9ebb59637c6842f7f81f901194a4bc850
ZianiAbde <abdelkader.ziani@outlook.com>
2020-09-23 18:03:20 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/sfgDi/controller/MyController.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgDi.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by abdelkader on 09/23/2020.
+ */
+@Controller
+public class MyController{
+
+public String sayHello(){
+
+    System.out.println("Hello World !!!!");
+
+    return "Hi Lyanna";
+}
+
+
+
+
+}
+
+
+
+
+
+
