367bf036027113be84ababc86d001a1f0f7b0999
ZianiAbde <abdelkader.ziani@outlook.com>
2020-11-09 07:35:18 -0500

    Qualifier spring property.

---
+++ src/main/java/guru/springframework/sfgDi/services/PrimaryGreetingService.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgDi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by abdelkader on 11/09/2020.
+ */
+@Primary
+@Service
+public class PrimaryGreetingService implements GreetingService {
+
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World! - From The Primary Bean";
+    }
+}
